25a10d6701819d89849d3e6be460cb9db25e4dcc
boxden <denbox030@gmail.com>
2024-05-04 13:02:03 +0300

    Remove source code from Hammer Units Converter 2.2

--- Programs/Hammer Units Converter 2.2/Source code/Starter.java
+++
@@ -1,9 +0,0 @@
-package HUC;
-
-public class Starter{
-	public static void main	(String[] args){
-		Main mainWindow = new Main();
-                mainWindow.setResizable(false);
-		mainWindow.setVisible(true);
-	}
-}
