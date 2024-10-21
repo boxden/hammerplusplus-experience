f851cec8ac3d16286d35577d5ce16d5d0dac7be9
boxden <denbox030@gmail.com>
2024-10-21 23:57:34 +0300

    Update BSPSource 1.4.5 -> BSPSource 1.4.6.1

--- Programs/bspsrc 1.4.5/bspinfo.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start "" "%~dp0\runtime\bin\javaw" -cp "%~dp0\bspsrc.jar" info.ata4.bspsrc.app.info.BspInfo %*
--- Programs/bspsrc 1.4.5/bspsrc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if "%~1" == "" (
-	start "" "%~dp0\runtime\bin\javaw" -jar "%~dp0\bspsrc.jar" %*
-) else (
-	"%~dp0\runtime\bin\java" -jar "%~dp0\bspsrc.jar" %*
-)
